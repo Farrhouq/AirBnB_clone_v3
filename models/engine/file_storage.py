3ff7377ab244a1f622555853424cba9438ed6f9b
Farrhouq <farouqimoro@gmail.com>
2024-06-10 22:04:49 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,26 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves an object
+
+        Args:
+            cls (class): class object
+            id (str): string representing the object ID
+        """
+        all_cls = self.all(cls)
+        for key, val in all_cls.items():
+            if key.split(".")[1] == id:
+                return val
+        return None
+
+    def count(self, cls=None):
+        """counts the number of objects in storage
+        
+        Args:
+            cls (classs): class object
+        """
+        if cls is not None:
+            return len(self.all(cls))
+        return len(self.all())
